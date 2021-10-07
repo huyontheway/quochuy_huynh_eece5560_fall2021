9fe307d3eb07292a4ac0fb00499043806566222c
huyontheway <huynhquochuy688@gmail.com>
2021-10-07 13:07:22 -0400

    homework4

--- packages/hw2_package/src/listener.py
+++ packages/hw2_package/src/listener.py
@@ -6,9 +6,19 @@ from std_msgs.msg import Float32
 class Listener:
     def __init__(self):
         rospy.Subscriber("/mystery/output1", Float32, self.callback)
+        
 
     def callback(self,msg):
-        rospy.loginfo("/mystery/output1 published %f", msg.data)
+        
+        if rospy.has_param("/homework4/feet"):
+            rospy.loginfo("/mystery/output1 published %f feet", msg.data)
+
+        elif rospy.has_param("/homework4/smoots"):
+            rospy.loginfo("/mystery/output1 published %f smoots", msg.data)
+
+        else:
+            rospy.loginfo("/mystery/output1 published %f meters", msg.data)
+
 
 if __name__ == '__main__':
     rospy.init_node('listener',anonymous=True)
